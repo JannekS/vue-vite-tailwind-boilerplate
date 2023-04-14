690587d95bbda7a7fee6ac08878f8032513942fb
Jannek <janneks@tutanota.com>
2023-04-14 13:38:22 +0200

    Added example code to App.vue to check setup.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div class="w-96 h-screen mx-auto bg-green-400">
+    <h2 class="p-8 text-center text-2xl font-bold text-slate-700">Hello Vue World!</h2>
+  </div>
 </template>
 
 <script></script>
